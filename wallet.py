047470b3111825a05d623056bcce8160ee7b4684
Rachael Brady <rachael.e.brady@gmail.com>
2023-02-09 11:43:20 -0500

    changed private key output to wif

--- wallet.py
+++ wallet.py
@@ -3,4 +3,10 @@ from bitsv import Key, PrivateKey
 network = "main"
 my_key_test = PrivateKey(network=network)
 print(my_key_test.address)
-print(my_key_test.to_hex())
+#https://austecon.github.io/bitsv/guide/keys.html#wif
+#Wif is more common for your output of your private key
+print(my_key_test.to_wif())
+
+#https://austecon.github.io/bitsv/guide/keys.html#hex
+#This is the website to the bitsv libary which creates the wallets and keys. Most don't use HEX but uncomment line 10 if you want hex.
+#print(my_key_test.to_hex())
